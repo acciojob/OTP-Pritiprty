358483a74037e2959c7c42f838a5af9122ac807b
Priti Rai <138751304+Pritiprty@users.noreply.github.com>
2024-08-06 06:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, index) => {
+    code.addEventListener('keyup', (e) => {
+        if (e.target.value.length === 1) {
+            if (index < codes.length - 1) {
+                codes[index + 1].focus();
+            }
+        } else if (e.key === 'Backspace' && index > 0) {
+            codes[index - 1].focus();
+            codes[index - 1].select();
+        }
+    });
+});
